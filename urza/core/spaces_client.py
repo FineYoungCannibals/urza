8c20c914db1ff8efd7ca59579b4e999463e3cab8
Finleyh <finley.d.hammond@outlook.com>
2026-01-30 11:42:43 -0500

    added test for dospaces

---
+++ urza/core/spaces_client.py
@@ -0,0 +1,61 @@
+from urza.config.settings import DO_TOKEN, DO_BASE_URL
+import httpx
+import boto3
+
+class DOAgent():
+    def __init__(self):
+        self.do_token = DO_TOKEN
+        self.headers = {'Content-Type':'application/json', \
+            'Authorization':f'Bearer {self.do_token}'}
+    
+    def test_connection(self):
+        '''
+        Docstring for test_connection
+        @returns True, able to authenitcate to DO API using keys 
+        '''
+        try:
+            with httpx.Client(base_url=DO_BASE_URL, headers=self.headers) as client:
+                response = client.get('/spaces/keys')
+                response.raise_for_status()
+                data = response.json()
+                print(data)
+    
+    
+                response = client.get('/spaces')
+                response.raise_for_status()
+                data = response.json()
+                print(data)
+                return data
+        except Exception as e:
+            print(f"Error listing spaces: {str(e)}")
+            return None
+
+    
+    def list_keys(self):
+        '''
+        Docstring for list_keys
+        @returns True, able to authenitcate to DO API using keys 
+        '''
+        with httpx.Client(base_url=DO_BASE_URL, headers=self.headers) as client:
+            response = client.get('/spaces/keys')
+            print(response.json())
+        return response.json()
+    
+    def create_spaces_keys(self):
+        '''
+        @return str: do spaces keys
+        '''
+        print("TODO")
+        return
+    
+    
+    def revoke_spaces_keys(self, old_keys):
+        '''
+        Docstring for revoke_spaces_keys
+        
+        :param self: Description
+        :param old_keys: Description
+        :return new_keys
+        '''
+        print("TODO")
+        return
